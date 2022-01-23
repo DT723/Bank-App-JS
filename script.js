314aadd41b7b4ea553630bf56f0610f08c1dfc07
DT723 <davidtesno@gmail.com>
2022-01-23 11:11:50 -0500

    demo selection styling

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 // Data
 const account1 = {
-  owner: 'Dave t',
+  owner: 'User1',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -26,7 +26,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Rich b',
+  owner: 'User2',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
